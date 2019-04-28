b928cc45daf2606511d131840ee1d95d34e76bc1
tirans3 <tirans3@mail.ru>
2019-04-28 21:52:47 +0400

    Add new class

---
+++ visualTodoList/Task.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace visualTodoList
+{
+    class Task
+    {
+        public string task { get; set; }
+
+        public bool Completed { get; set; } = false;
+
+        public Task(string task)
+        {
+            this.task = task;
+
+        }
+    }
+}
