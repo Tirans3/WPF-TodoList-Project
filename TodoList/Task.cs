3d10201602578a59eac64beea34645f5ce0ea67c
tirans3 <tirans3@mail.ru>
2019-04-28 20:03:49 +0400

    Change class name

---
+++ TodoList/Task.cs
@@ -0,0 +1,16 @@
+﻿namespace TodoList
+{
+    public class Task
+    {
+        public string task { get; set; }
+
+        public bool Completed { get; set; } = false;
+
+        public Task(string taskdesc)
+        {
+            task = taskdesc;
+
+        }
+
+    }
+}
