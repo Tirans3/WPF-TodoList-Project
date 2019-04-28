3c4cd422bf0d2dd1de5f18fc0a17b880f2771390
tirans3 <tirans3@mail.ru>
2019-04-28 21:53:03 +0400

    Add new class

---
+++ visualTodoList/ToDoList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace visualTodoList
+{
+    class ToDoList
+    {
+        class TodoList
+        {
+            readonly List<Task> todo;
+
+            public TodoList()
+            {
+                todo = new List<Task>();
+            }
+
+            public void Add(Task task)
+            {
+                todo.Add(task);
+            }
+
+            public void Remove(Task task)
+            {
+                todo.Remove(task);
+            }
+
+            public void Remove(int i)
+            {
+                todo.RemoveAt(i);
+            }
+
+            public int RemoveAll(Predicate<Task> match)
+            {
+                return todo.RemoveAll(match);
+            }
+
+            public void Complete(int i)
+            {
+                todo[i].Completed = true;
+            }
+
+            public void CompleteAll(Predicate<Task> match)
+            {
+                for (int i = 0; i < todo.Count; i++)
+                {
+
+                    if (match(todo[i]) == true)
+                    {
+                        todo[i].Completed = true;
+                    }
+
+                }
+            }
+        }
+    }
+}
