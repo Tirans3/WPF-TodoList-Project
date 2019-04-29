957ebb062dcc2e1b73ac99be7c383802de966ef6
tirans3 <tirans3@mail.ru>
2019-04-29 14:04:21 +0400

    Add new property Count

--- visualTodoList/ToDoList.cs
+++ visualTodoList/ToDoList.cs
@@ -1,62 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace visualTodoList
 {
     class ToDoList
     {
-            readonly List<Task> todo;
+        readonly List<Task> todo;
 
-            public ToDoList()
-            {
-                todo = new List<Task>();
-            }
+        public ToDoList()
+        {
+            todo = new List<Task>();
+        }
 
-            public void Add(Task task)
-            {
-                todo.Add(task);
-            }
+        public int Count { get => todo.Count; }
+
+        public void Add(Task task)
+        {
+            todo.Add(task);
+        }
 
         public void Add(string str)
         {
             Add(new Task(str));
         }
 
-            public void Remove(Task task)
-            {
-                todo.Remove(task);
-            }
+        public void Remove(Task task)
+        {
+            todo.Remove(task);
+        }
 
-            public void Remove(int i)
-            {
-                todo.RemoveAt(i);
-            }
+        public void Remove(int i)
+        {
+            todo.RemoveAt(i);
+        }
 
-            public int RemoveAll(Predicate<Task> match)
-            {
-                return todo.RemoveAll(match);
-            }
+        public int RemoveAll(Predicate<Task> match)
+        {
+            return todo.RemoveAll(match);
+        }
 
-            public void Complete(int i)
-            {
-                todo[i].Completed = true;
-            }
+        public void Complete(int i)
+        {
+            todo[i].Completed = true;
+        }
 
-            public void CompleteAll(Predicate<Task> match)
+        public void CompleteAll(Predicate<Task> match)
+        {
+            for (int i = 0; i < todo.Count; i++)
             {
-                for (int i = 0; i < todo.Count; i++)
+
+                if (match(todo[i]) == true)
                 {
-
-                    if (match(todo[i]) == true)
-                    {
-                        todo[i].Completed = true;
-                    }
-
+                    todo[i].Completed = true;
                 }
+
             }
-        
+        }
+
     }
 }
